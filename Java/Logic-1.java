cb0bf93f463033d54bcb8901a5213162a3c3f29e
ruchika555jain <ruchika555jain@gmail.com>
2017-06-09 14:25:39 +0530

    Create Logic-1.java

--- Java/Logic-1.java
+++ Java/Logic-1.java
@@ -314,6 +314,15 @@ public int numOfDigits(int num)
 	return count;
 }
 
+//fizzString
+public String fizzString(String str) {
+  if(str.startsWith("f")&&str.endsWith("b")) return "FizzBuzz";
+    if(str.startsWith("f")) return "Fizz";
+  if(str.endsWith("b")) return "Buzz";
+  
+  return str;
+}
+
 
 
 
